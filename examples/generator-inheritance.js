c2e9bd0d05b183d3d51ae56d8c04770f37e1b6fd
Kaique da Silva <kaeyosthaeron@gmail.com>
2019-03-04 10:27:39 -0300

    Dealing with some mutaces based on their conditionalities For a symbol with a multiple assignments without mutation

--- examples/generator-inheritance.js
+++ examples/generator-inheritance.js
@@ -1,10 +1,10 @@
 const xmatcher = require('../');
 
-console.log(xmatcher.matches((async function () {
+!!!!xmatcher.matches((async function () {
     return await function* self() {
         this.example = 'some-value';
         return this;
     }
 })()), new xmatcher.Symbol('some-value', v => {
     return xmatcher.includes(v, global['matcherPatternValue']);
-}));
+});
